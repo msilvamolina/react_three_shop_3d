4331234a113464b3fe8ac67109e41c3a982407ac
Martín Silva Molina <msilvamolina@gmail.com>
2024-02-27 05:32:31 +0100

    ya pusimos el modelo 3d y funciona!

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useRef } from "react";
 import {Canvas} from '@react-three/fiber'
-import {OrbitControls, Center} from '@react-three/drei'
+import {OrbitControls, Center, useGLTF} from '@react-three/drei'
 
 export const App = ({position=[-1, 0, 2.5], fov=25}) => {
+
   return (
     <Canvas 
         eventSource={document.getElementById('root')}
@@ -16,11 +17,22 @@ export const App = ({position=[-1, 0, 2.5], fov=25}) => {
   )
 }
 
-function Shirt() {
+function Shirt(props) {
+    const { nodes, materials } = useGLTF("/buzo_starter.glb");
+
     return (
-        <mesh>
-            <boxGeometry args={[0.9, 0.9, 0.9]} />
-            <meshNormalMaterial />
-        </mesh>
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.buzo.geometry}
+        material={materials.Knit_Fleece_Terry_FRONT_54054}
+        position={[0, 4.829, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+      />
+    </group>
     )
 }
+
+useGLTF.preload("/buzo_starter.glb");
+
