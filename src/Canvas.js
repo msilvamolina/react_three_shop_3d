133afa7b27690a2ec4985fe3cd4dea2c2d87a0b9
Martín Silva Molina <msilvamolina@gmail.com>
2024-02-27 05:47:57 +0100

    tenemos el fondo y lo tenemos centrado

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,6 +1,6 @@
 import React, { useRef } from "react";
 import {Canvas} from '@react-three/fiber'
-import {OrbitControls, Center, useGLTF, Environment} from '@react-three/drei'
+import {OrbitControls, Center, useGLTF, Environment, AccumulativeShadows, RandomizedLight} from '@react-three/drei'
 
 export const App = ({position=[-1, 0, 2.5], fov=25}) => {
 
@@ -13,6 +13,7 @@ export const App = ({position=[-1, 0, 2.5], fov=25}) => {
         <Environment preset="city"/>
         <Center>
             <Shirt />
+            <Backdrop />
         </Center>
         <OrbitControls />
     </Canvas>
@@ -25,16 +26,36 @@ function Shirt(props) {
     return (
     <group {...props} dispose={null}>
       <mesh
+      scale={[0.10, 0.10, 0.10]}
         castShadow
         receiveShadow
         geometry={nodes.buzo.geometry}
         material={materials.Knit_Fleece_Terry_FRONT_54054}
-        position={[0, 4.829, 0]}
+        position={[0, 0, 0]}
         rotation={[Math.PI / 2, 0, 0]}
       />
     </group>
     )
 }
 
+function Backdrop() {
+    return(
+        <AccumulativeShadows
+        temporal
+        frames={60}
+        alphaTest={0.85}
+        scale={10}
+        rotation={[Math.PI / 2, 0, 0]}
+        position={[0,0,-0.14]}>
+            <RandomizedLight
+                amount={4}
+                radius={9}
+                intensity={1}
+                ambient={0.25}
+                position={[5, 5, -10]}
+            />
+        </AccumulativeShadows>
+    )
+}
 useGLTF.preload("/buzo_starter.glb");
 
