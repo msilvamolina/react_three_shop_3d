d8de9b9e8b47502b3350741a0d82624829a29adb
Martín Silva Molina <msilvamolina@gmail.com>
2024-02-27 05:19:44 +0100

    ya tenemos un super cubo

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import {Canvas} from '@react-three/fiber'
+import {OrbitControls, Center} from '@react-three/drei'
 
-export const App = () => {
+export const App = ({position=[-1, 0, 2.5], fov=25}) => {
   return (
-    <Canvas>
-        <Shirt />
+    <Canvas 
+        eventSource={document.getElementById('root')}
+        eventPrefix='client'
+        camera={{position, fov}} >
+        <Center>
+            <Shirt />
+        </Center>
+        <OrbitControls />
     </Canvas>
   )
 }
