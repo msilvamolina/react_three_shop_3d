7066e6c938bcb4ea61a6e59e39103ab7d2beb242
Martín Silva Molina <msilvamolina@gmail.com>
2024-02-28 05:16:10 +0100

    vamos a seguir con este porque el buzo no tiene decal

--- src/Canvas.js
+++ src/Canvas.js
@@ -38,22 +38,18 @@ function Shirt(props) {
 
   const texture = useTexture(`/${snap.selectedDecal}.png`)
 
-    const { nodes, materials } = useGLTF("/buzo_starter.glb");
+  const { nodes, materials } = useGLTF('/shirt_baked_collapsed.glb')
 
   useFrame((state, delta) =>
-    easing.dampC(materials.Knit_Fleece_Terry_FRONT_54054.color, snap.selectedColor, 0.25, delta)
+    easing.dampC(materials.lambert1.color, snap.selectedColor, 0.25, delta)
   )
 
   return (
     <mesh
-        castShadow
-        receiveShadow
-      geometry={nodes.buzo.geometry}
-      material={materials.Knit_Fleece_Terry_FRONT_54054}
+      castShadow
+      geometry={nodes.T_Shirt_male.geometry}
+      material={materials.lambert1}
       material-roughness={1}
-      rotation={[Math.PI / 2, 0, 0]}
-            scale={[0.10, 0.10, 0.10]}
-
       {...props}
       dispose={null}>
       <Decal
@@ -121,4 +117,4 @@ function CameraRig({ children }) {
   return <group ref={group}>{children}</group>
 }
 
-useGLTF.preload('/buzo_starter.glb')
+useGLTF.preload('/shirt_baked_collapsed.glb')
