ba3f2fe34a2d9602d47815d8abf27e8f18242526
Martín Silva Molina <msilvamolina@gmail.com>
2024-02-28 03:07:53 +0100

    trabajamos en la pantalla

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,6 +1,6 @@
 
 import { Logo } from '@pmndrs/branding'
-import { AiOutlineHighLight, AiOutlineShopping } from 'react-icons/ai'
+import { AiOutlineHighlight, AiOutlineShopping } from 'react-icons/ai'
 
 export default function Overlay() {
     return <Intro />
@@ -12,5 +12,23 @@ function Intro() {
             <Logo width="40" height="40" />
             <AiOutlineShopping size="3em" />
         </header>
+
+        <section key="main">
+            <div className="section--container">
+                <div>
+                    <h1>LET'S DO IT.</h1>
+                </div>
+                <div className="section--content">
+                    <div>
+                        <p>
+                            Create your own custom clothing with our 3D configurator.
+                        </p>
+                        <button style={{background: 'black'}}>
+                            CUSTOMIZE IT <AiOutlineHighlight size="1.3em"/> 
+                        </button>
+                    </div>
+                </div> 
+            </div>
+        </section>
     </div>
 }
