1e244a83e2af633a6e427dc5b34d0ecbbc016da7
Martín Silva Molina <msilvamolina@gmail.com>
2024-02-28 04:44:03 +0100

    pusimos el snap

--- src/Overlay.js
+++ src/Overlay.js
@@ -1,4 +1,3 @@
-
 import { Logo } from '@pmndrs/branding'
 import {
   AiOutlineHighlight,
@@ -6,80 +5,91 @@ import {
   AiFillCamera,
   AiOutlineArrowLeft
 } from 'react-icons/ai'
-
 import { useSnapshot } from 'valtio'
 import { state } from './store'
 
 export default function Overlay() {
-    const snap = useSnapshot(state)
-    return (
-        <div className="container">
-            <header>
-                <Logo width="40" height="40" />
-                <AiOutlineShopping size="3em" />
-            </header>
-            {snap.intro ? <Intro /> : <Customizer />}
-           
+  const snap = useSnapshot(state)
+
+  return (
+    <div className="container">
+      <header>
+        <Logo width="40" height="40" />
+        <div>
+          <AiOutlineShopping size="3em" />
         </div>
-        )
+      </header>
+
+      {snap.intro ? <Intro /> : <Customizer />}
+    </div>
+  )
 }
 
 function Intro() {
-    return <section key="main">
-            <div className="section--container">
-                <div>
-                    <h1>LET'S DO IT.</h1>
-                </div>
-                <div className="section--content">
-                    <div>
-                        <p>
-                            Create your own custom clothing with our 3D configurator.
-                        </p>
-                        <button style={{background: 'black'}} onClick={()=>{state.intro = false}}>
-                            CUSTOMIZE IT <AiOutlineHighlight size="1.3em"/> 
-                        </button>
-                    </div>
-                </div> 
-            </div>
-        </section>
+  return (
+    <section key="main">
+      <div className="section--container">
+        <div>
+          <h1>LET'S DO IT.</h1>
+        </div>
+        <div className="support--content">
+          <div>
+            <p>
+              Create your unique and exclusive shirt with our brand-new 3D
+              customization tool. <strong>Unleash your imagination</strong> and
+              define your own style.
+            </p>
+            <button
+              style={{ background: 'black' }}
+              onClick={() => (state.intro = false)}>
+              CUSTOMIZE IT <AiOutlineHighlight size="1.3em" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 function Customizer() {
-  const colors = [
-    '#ccc',
-    '#EFBD4E',
-    '#80C670',
-    '#726DE8',
-    '#EF674E',
-    '#353934',
-    'Purple'
-  ]
+  const snap = useSnapshot(state)
+
+  const colors = ['#ccc', '#EFBD4E', '#80C670', '#726DE8', '#EF674E', '#353934']
   const decals = ['react', 'three2', 'pmndrs']
-    return <section key="custom">
-        <div className='customizer'>
-            <div className='color-options'>
-                {colors.map(color => 
-                <div 
-                    key={color}
-                    className='circle'
-                    style={{background: color}}></div>
-                )}
-            </div>
+
+  return (
+    <section key="custom">
+      <div className="customizer">
+        <div className="color-options">
+          {colors.map((color) => (
+            <div
+              key={color}
+              className="circle"
+              style={{ background: color }}
+              onClick={() => (state.selectedColor = color)}></div>
+          ))}
         </div>
-        <div className='decals'>
-            <div className='decals--container'>
-                {decals.map(decal =>(
-                    <div key={decal} className='decal'>
-                        <img src={decal + '_thumb.png'} alt="brand" />
-                    </div>
-                ))}
-            </div>
+        <div className="decals">
+          <div className="decals--container">
+            {decals.map((decal) => (
+              <div key={decal} className="decal">
+                <img src={decal + '_thumb.png'} alt="brand" />
+              </div>
+            ))}
+          </div>
         </div>
-        <button className='share' style={{background: 'black'}}>
-            DOWNLOAD <AiFillCamera size="1.3em"/>
+        <button className="share" style={{ background: snap.selectedColor }}>
+          DOWNLOAD
+          <AiFillCamera size="1.3em" />
         </button>
-        <button className='exit' style={{background: 'black'}} onClick={()=>{state.intro = true}}>
-            GO BACK <AiOutlineArrowLeft size="1.3em"/>
-        </button>        
+        <button
+          className="exit"
+          style={{ background: snap.selectedColor }}
+          onClick={() => (state.intro = true)}>
+          GO BACK
+          <AiOutlineArrowLeft size="1.3em" />
+        </button>
+      </div>
     </section>
+  )
 }
